61f8cbdae5324b87a60f527107ddf8173ea99bd0
maxpleaner <maxpleaner@gmail.com>
2016-03-12 15:31:37 -0800

    typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 
 root_url = Rails.env.production? ? "maxp-tickers.herokuapp.com" : "localhost:3000"
-self.create(
+Ticker.create(
   name: "test",
   interval: 1000,
   content: "`curl \"#{root_url}/update_output?name=test&password=#{ENV["password"]}&output=\#{SecureRandom.urlsafe_base64}\"`\r\n\r\n# do not use \"exit\"\r\n# and do not raise any errors, or zombie processes may result\r\n"
